80405ff2f9d3ee563580c9a2f4d850b3a042e501
joelrenish <joel.renish@mewurk.com>
2024-01-23 21:12:35 -0800

    finished all the test cases

--- src/main/kotlin/edu/uw/complexkotlin/Library.kt
+++ src/main/kotlin/edu/uw/complexkotlin/Library.kt
@@ -9,7 +9,16 @@ package edu.uw.complexkotlin
 // the final string should look like FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ for 0..15.
 // store this lambda into 'fizzbuzz' so that the tests can call it
 //
-val fizzbuzz : (IntRange) -> String = { "" }
+val fizzbuzz : (IntRange) -> String = { range ->
+    range.map {
+        when {
+            it % 3 == 0 && it % 5 == 0 -> "FIZZBUZZ"
+            it % 3 == 0 -> "FIZZ"
+            it % 5 == 0 -> "BUZZ"
+            else -> ""
+        }
+    }.fold("") {result, new -> result + new }
+}
 
 // Example usage
 /*
@@ -34,19 +43,46 @@ fun process(message: String, block: (String) -> String): String {
     return ">>> ${message}: {" + block(message) + "}"
 }
 // Create r1 as a lambda that calls process() with message "FOO" and a block that returns "BAR"
-val r1 = { }
+val r1 = { process("FOO") { "BAR" } }
 
-// Create r2 as a lambda that calls process() with message "FOO" and a block that upper-cases 
+// Create r2 as a lambda that calls process() with message "FOO" and a block that upper-cases
 // r2_message, and repeats it three times with no spaces: "WOOGAWOOGAWOOGA"
 val r2_message = "wooga"
-val r2 = { }
-
+val r2 = { process("FOO") { (r2_message.toUpperCase().repeat(3)) } }
 
 // write an enum-based state machine between talking and thinking
-enum class Philosopher { }
+// enum class Philosopher { }
+enum class Philosopher {
+    THINKING {
+        override fun toString(): String { return "Deep thoughts...." }
+    },
+    TALKING {
+        override fun toString(): String { return "Allow me to suggest an idea..." }
+    };
+
+    fun signal(): Philosopher {
+        return when (this) {
+            THINKING -> Philosopher.TALKING
+            TALKING -> Philosopher.THINKING
+        }
+    }
+}
+
+/*
+EXTRA CREDIT :
+
+Seneca the Younger was a Roman philosopher who followed the school of Stoicism. His career was very successful as
+he ended up tutoring and advising Emperor Nero for a long time, but eventually Nero ordered him to kill himself
+because Seneca may have plotted to kill him.
+
+To summarize Stoicism, it's basically about always being virtuous so that you can have a happy, meaningful life,
+and the main virtues they focus on are wisdom, courage, temperance, and justice.
+
+*/
+
 
 // create an class "Command" that can be used as a function.
-// To do this, provide an "invoke()" function that takes a 
+// To do this, provide an "invoke()" function that takes a
 // single parameter ("message" of type String). Command's
 // primary constructor should take a String argument ("prompt").
 // When invoked, the Command object should return a String
@@ -55,4 +91,9 @@ enum class Philosopher { }
 // val cmd = Command(": ")
 // val result = cmd("Hello!")
 // result should equal ": Hello!"
-class Command(val prompt: String) { }
+
+class Command(val prompt: String) {
+    operator fun invoke(message: String): String {
+        return prompt + message
+    }
+}
